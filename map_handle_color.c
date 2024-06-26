cd8e74912247161f382f01b65ebf6102cc5d6bf7
aismaili10 <ismailialireza15@gmail.com>
2024-06-26 20:59:23 +0200

    RGB cases covered, keep testing

--- map_handle_color.c
+++ map_handle_color.c
@@ -6,7 +6,7 @@
 /*   By: aismaili <aismaili@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 14:05:58 by aismaili          #+#    #+#             */
-/*   Updated: 2024/06/26 18:22:45 by aismaili         ###   ########.fr       */
+/*   Updated: 2024/06/26 20:50:19 by aismaili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,14 @@ static int	rgb_valid(char *rgb)
 	j = 0;
 	while (rgb[j] && rgb[j] != '\n')
 	{
+		if (j == 0 && ft_strchr("+-", rgb[j]))
+		{
+			j++;
+			continue ;
+		}
 		if (!ft_isdigit(rgb[j]))
 		{
-			//printf("rgb: %s --> rgb[%i]: %c\n", rgb, j, rgb[j]);
+			//printf("rgb: -%s- --> rgb[%i]: -%c-\n", rgb, j, rgb[j]);
 			return (false);
 		}
 		j++;
@@ -29,13 +34,31 @@ static int	rgb_valid(char *rgb)
 	return (true);
 }
 
+char	*rm_zero(char *num)
+{
+	int	i;
+
+	i = 0;
+	if (ft_strchr("+-", num[i]))
+		i++;
+	while (num[i] == '0' && i < (int)ft_strlen(num) - 1)
+		i++;
+	return (&num[i]);
+}
+
 static int	inv_clr_specifier(char **str_rgb)
 {
 	int	i;
 
 	i = 0;
+	//char	*zeros_case;
 	while (str_rgb[i])
 	{
+		if (ft_strlen(rm_zero(str_rgb[i])) > 4)
+		{
+			//printf("bigger then 3\n%zu\n-%s-\n", ft_strlen(rm_zero(str_rgb[i])), str_rgb[i]);			
+			return (write(2, COLOR_RED"ISSUE: RGB Range\n"COLOR_RESET, 29),true);
+		}
 		if (!rgb_valid(str_rgb[i]))
 		{
 			//printf("inv_clr_specifier(): str_rgb[%i]: %s\n", i, str_rgb[i]);
@@ -46,6 +69,45 @@ static int	inv_clr_specifier(char **str_rgb)
 	return (false);
 }
 
+int	inv_range(int *rgb)
+{
+	int	i;
+
+	i = 0;
+	while (i < 3)
+	{
+		if (rgb[i] < 0 || rgb[i] > 255)
+			return (true);
+		i++;
+	}
+	return (false);
+}
+
+int	comma_check(char *str)
+{
+	int	i;
+	int	comma_counter;
+
+	i = 0;
+	comma_counter = 0;
+	while (str[i])
+	{
+		if (str[i] == ',')
+			comma_counter++;
+	// position of commas
+		if (str[i] == ',' && str[i + 1] == ',')
+			return (write(2, COLOR_RED"ISSUE: Consecutive Commas\n"COLOR_RESET, 38), INV_MAP);
+		i++;
+	}
+	// number of commas
+	if (comma_counter != 2)
+		return (write(2, COLOR_RED"ISSUE: NUM of Commas\n"COLOR_RESET, 33), INV_MAP);
+	// beginning and end
+	if (str[0] == ',' || str[ft_strlen(str) - 1] == ',')
+		return (write(2, COLOR_RED"ISSUE: Consecutive Commas\n"COLOR_RESET, 38), INV_MAP);
+	return (SUCCESS);
+}
+
 int	init_u_map_clr(t_map *u_map)
 {
 	char	**str_rgb;
@@ -54,6 +116,8 @@ int	init_u_map_clr(t_map *u_map)
 
 	errno = 0;
 	//printf("in init_mapclr: %s\n", u_map->splited_line[1]);
+	if (comma_check(u_map->splited_line[1]))
+		return (write(2, COLOR_RED"RGB Invalid Map\n"COLOR_RESET, 28), INV_MAP);
 	str_rgb = ft_split(u_map->splited_line[1], ',');
 	if (!str_rgb && errno)
 		return (perror("malloc"), SYS_FAIL);
@@ -68,6 +132,8 @@ int	init_u_map_clr(t_map *u_map)
 	while (++i < 3)
 		rgb[i] = ft_atoi(str_rgb[i]); // handle overflow!!
 	free_str_array(&str_rgb);
+	if (inv_range(rgb))
+		return (write(2, COLOR_RED"RGB Invalid Range\n"COLOR_RESET, 30), INV_MAP);
 	i = ft_strlen(u_map->splited_line[0]);
 	if (!ft_strncmp(u_map->splited_line[0], "C", i))
 		u_map->c_color = (rgb[0] << 16) | (rgb[1] << 8) | rgb[2]; // gotta understand how exactly that works
